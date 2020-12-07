64c4fd36fc3a6cf93b8c04a361f8e2c4133e3993
nishat-mumu <73047122+nishat-mumu@users.noreply.github.com>
2020-12-08 00:50:40 +0600

    Add files via upload

---
+++ final/final-lab_5/Final Labtask_5/design.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>CSS Grid Responsive Site</title>
+</head>
+
+<body>
+    <header id="showcase" class="grid">
+        <div class="bg-image"></div>
+        <div class="content-wrap">
+            <h1>Welcome to Food Mania</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eum error earum soluta voluptatum
+                nisi laboriosam eos saepe asperiores dolorum.</p>
+        </div>
+    </header>
+
+    <main id="main">
+        <section id="section-a" class="grid">
+            <div class="content-wrap">
+                <h2 class="content-title">What is Food Mania</h2>
+                <div class="content-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde
+                        officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus.
+                        Perferendis officiis deserunt velit voluptas nobis sequi
+                        animi totam, accusantium, ex eius quia, natus quo?</p>
+                </div>
+            </div>
+        </section>
+        <section id="section-b" class="grid">
+            <ul>
+                <li>
+                    <div class="card">
+                        <img src="2.jpg" alt="">
+                        <div class="card-content">
+                            <h3 class="card-title">Delicious Food</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et
+                                quasi velit voluptatum cum maiores exercitationem.</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <img src="3.jpg" alt="">
+                        <div class="card-content">
+                            <h3 class="card-title">Online Order</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et
+                                quasi velit voluptatum cum maiores exercitationem.</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <img src="4.jpg" alt="">
+                        <div class="card-content">
+                            <h3 class="card-title">Free Delivery</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et
+                                quasi velit voluptatum cum maiores exercitationem.</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </section>
+
+        <section id="section-c" class="grid">
+            <div class="content-wrap">
+                <h2 class="content-title">Food is Everything</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nam rerum vel earum error fugiat
+                    cupiditate, dolore eius! Minus, explicabo.</p>
+            </div>
+        </section>
+
+        <section id="section-d" class="grid">
+            <div class="box">
+                <h2 class="content-title">Contact Us</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, suscipit. Rerum ducimus a quod, ut et
+                    voluptas obcaecati unde fuga.</p>
+                <p>contact@nishat@gmail.com</p>
+            </div>
+            <div class="box">
+                <h2 class="content-title">About Our Policy</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio earum porro deserunt, deleniti,
+                    quae facere repudiandae, officiis est exercitationem nobis iusto doloremque! Soluta excepturi in
+                    aut suscipit amet temporibus quo?</p>
+            </div>
+        </section>
+    </main>
+    <footer id="main-footer" class="grid">
+        <div>Food Mania</div>
+        <div>Project By <a href="#" target="_blank">Nishat Tasnim</a></div>
+    </footer>
+
+</body>
+
+</html>
